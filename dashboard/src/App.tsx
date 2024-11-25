d3fc126def29cae825234042a526166e51d9d192
mcpoveda2 <mcpoveda@espol.edu.ec>
2024-11-25 14:15:02 -0500

    Cambios en la tabla

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,6 +2,9 @@
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 import './App.css'
+import IndicatorWeather from './components/IndicatorWeather';
+import TableWeather from './components/TableWeather';
+import ControlWeather from './components/ControlWeather';
 
 import Grid from '@mui/material/Grid2' 
 
@@ -13,13 +16,31 @@ function App() {
       <Grid  container spacing={5}>
 
        	  {/* Indicadores */}
-           <Grid size={{ xs: 12, xl: 3}}>Elemento: Indicador 1</Grid>
-           <Grid size={{ xs: 12, xl: 3}}>Elemento: Indicador 2</Grid>
-           <Grid size={{ xs: 12, xl: 3}}>Elemento: Indicador 3</Grid>
-           <Grid size={{ xs: 12, xl: 3}}>Elemento: Indicador 4</Grid>
+           <Grid size={{ xs: 12, xl: 3}}>
+           <IndicatorWeather title={'Indicator 1'} subtitle={'Unidad 1'} value={"1.23"} /> 
+            </Grid>
+           <Grid size={{ xs: 12, xl: 3}}>
+           <IndicatorWeather title={'Indicator 2'} subtitle={'Unidad 2'} value={"3.12"} />
+            </Grid>
+           <Grid size={{ xs: 12, xl: 3}}>
+           <IndicatorWeather title={'Indicator 3'} subtitle={'Unidad 3'} value={"2.31"} />
+            </Grid>
+           <Grid size={{ xs: 12, xl: 3}}>
+           <IndicatorWeather title={'Indicator 4'} subtitle={'Unidad 4'} value={"3.21"} />
+            </Grid>
 		      
            {/* Tabla */}
-           <Grid size={{ xs: 12, xl: 8}}>Elemento: Tabla</Grid>
+            <Grid size={{ xs: 12, xl: 8}}>
+            <Grid container spacing={2}>
+                     <Grid size={{ xs: 12, xl: 3 }}>
+                         <ControlWeather/>
+                     </Grid>
+                     <Grid size={{ xs: 12, xl: 9 }}>
+                         <TableWeather/>
+                     </Grid>
+                 </Grid>
+
+            </Grid>
 		      
            {/* Gráfico */}
            <Grid size={{ xs: 12, xl: 4}}>Elemento: Gráfico 1</Grid>
